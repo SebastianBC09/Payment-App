5c2f759762190729a7d819bf5e112447f9bcbca3
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-02-11 00:25:55 -0500

    feat(layout): setup base layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,13 +4,13 @@ import Header from './Header'
 
 const Layout:FC = () => {
   return (
-    <div className='min-h-screen bg-background'>
-      <Header/>
-      <main className='container mx-auto px-4 py-8'>
-        <Outlet/>
+    <div className="min-h-screen bg-white">
+      <Header />
+      <main className="max-w-5xl mx-auto px-4 py-8">
+        <Outlet />
       </main>
     </div>
-  )
+  );
 }
 
 export default Layout
