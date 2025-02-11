547b7fdd94d60bfc618839ca58bbc878f7839f5d
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-02-10 20:59:28 -0500

    feat: api file created

---
+++ src/services/api.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const BASE_URL = 'https://api-staging.muralpay.com';
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if(token){
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+)
