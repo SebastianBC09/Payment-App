a7a85dda4ee89158218ddbe2382d32e5d189338c
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-02-11 16:11:28 -0500

    feat: routes updated

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';;
 import { useAppDispatch } from './store/hooks';
 import Layout from './components/layout/Layout';
 import AccountCreation from './pages/AccountCreation';
 import AccountDashboard from './pages/AccountDashboard';
 import TransferPage from './pages/TransferPage';
 import TransactionsPage from './pages/TransactionsPage';
-// import { ProtectedRoute } from './components/auth/ProtectedRoute';
+import { ProtectedRoute } from './components/auth/ProtectedRoute';
 import { setCustomerSession } from './store/slices/customerSlice';
+import { fetchAccount } from './store/slices/accountSlice';
+
 
 function AppRoutes() {
   const dispatch = useAppDispatch();
@@ -17,6 +19,9 @@ function AppRoutes() {
     if (savedSession) {
       const session = JSON.parse(savedSession);
       dispatch(setCustomerSession(session));
+      if (session.accountId) {
+        dispatch(fetchAccount(session.accountId));
+      }
     }
   }, [dispatch]);
 
@@ -25,14 +30,12 @@ function AppRoutes() {
       <Route element={<Layout />}>
         <Route path="/create-account" element={<AccountCreation />} />
 
-        {/* <Route element={<ProtectedRoute>}> */}
           <Route path="/account" element={<AccountDashboard />} />
           <Route path="/transfer" element={<TransferPage />} />
           <Route path="/transactions" element={<TransactionsPage />} />
-        {/* </Route> */}
 
-        <Route path="/" element={<Navigate to="/create-account" replace />} />
-        <Route path="*" element={<Navigate to="/create-account" replace />} />
+        <Route path="/" element={<Navigate to="/account" replace />} />
+        <Route path="*" element={<Navigate to="/account" replace />} />
       </Route>
     </Routes>
   );
