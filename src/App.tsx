57851a80f16544325094099b82c9faff0b180cd8
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-02-11 00:30:11 -0500

    feat: component imported to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router';
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import Layout from './components/layout/Layout';
+import AccountCreation from './pages/AccountCreation';
 import './App.css'
 
 function App() {
-
   return (
     <Provider store={store}>
-      <Router>
-        <Routes>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<AccountCreation/>} />
+          {/* <Route path="/transfer" element={<TransferRequest/>}/> */}
+          {/* <Route path="/transactions" element={<Transactions/>}/> */}
+        </Route>
+      </Routes>
   </Provider>
   )
 }
