e48e799b093e9aa536ac2b4628c7fc61b88e5802
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-02-11 00:46:54 -0500

    feat(routing): add transactions route to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import Layout from './components/layout/Layout';
+import AccountDashboard from './pages/AccountDashboard';
 import AccountCreation from './pages/AccountCreation';
+import TransferPage from './pages/TransferPage';
+import TransactionsPage from './pages/TransactionsPage';
 import './App.css'
 
 function App() {
   return (
     <Provider store={store}>
       <Routes>
-        <Route element={<Layout/>}>
-          <Route path="/" element={<AccountCreation/>} />
-          {/* <Route path="/transfer" element={<TransferRequest/>}/> */}
-          {/* <Route path="/transactions" element={<Transactions/>}/> */}
+        <Route element={<Layout />}>
+          {/* Public route */}
+          <Route path="/create-account" element={<AccountCreation />} />
+
+          {/* Protected routes */}
+          <Route path="/account" element={<AccountDashboard />} />
+          <Route path="/transfer" element={<TransferPage />} />
+          <Route path="/transactions" element={<TransactionsPage />} />
+
+          {/* Redirects */}
+          <Route path="/" element={<Navigate to="/create-account" replace />} />
+          <Route path="*" element={<Navigate to="/create-account" replace />} />
         </Route>
       </Routes>
-  </Provider>
+    </Provider>
   )
 }
 
