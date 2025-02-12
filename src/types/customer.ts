83bd39aae58257a5d9e28e3356ee3db1adee90c2
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-02-12 12:57:37 -0500

    feat(types): extend BaseCustomer with account information

--- src/types/customer.ts
+++ src/types/customer.ts
@@ -1,6 +1,3 @@
-export const CUSTOMER_TYPES = ['BUSINESS', 'INDIVIDUAL'] as const;
-export type CustomerType = typeof CUSTOMER_TYPES[number];
-
 export const CUSTOMER_STATUSES = [
   'INACTIVE',
   'PENDING',
@@ -8,48 +5,27 @@ export const CUSTOMER_STATUSES = [
   'ERROR',
   'REJECTED'
 ] as const;
+
+export const CUSTOMER_TYPES = ['BUSINESS', 'INDIVIDUAL'] as const;
+
+export interface BaseCustomer {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  customerType: CustomerType;
+  status: CustomerStatus;
+}
+
 export type CustomerStatus = typeof CUSTOMER_STATUSES[number];
-export interface CustomerSession {
-  customerId: string;
+export type CustomerType = typeof CUSTOMER_TYPES[number];
+export type CustomerWithAccount = BaseCustomer & {
   accountId: string;
-  status: CustomerStatus;
-}
-export interface AccountBalance {
-  balance: number;
-  tokenSymbol: string;
-}
-export interface CustomerAccount {
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-  name: string;
-  address: string;
-  blockchain: string;
-  balance: AccountBalance;
-  isApiEnabled: boolean;
-  isPending: boolean;
-}
-interface BaseCustomer {
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-  name: string;
-  status: CustomerStatus;
+};
+
+export type CustomerSession = Pick<BaseCustomer, 'id' | 'status'> & {
   accountId: string;
-  account: CustomerAccount;
-}
-
-interface BusinessCustomer extends BaseCustomer {
-  customerType: 'BUSINESS';
-  businessTaxId?: string;
-}
-
-interface IndividualCustomer extends BaseCustomer {
-  customerType: 'INDIVIDUAL';
-  personalId?: string;
-}
-
-export type Customer = BusinessCustomer | IndividualCustomer;
+};
 
 export type CreateCustomerRequest = {
   name: string;
