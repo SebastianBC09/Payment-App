e71b181050da734a9480680095c9e09b35fb9171
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-02-12 13:01:10 -0500

    fix(types): add Account interface and update related types

--- src/types/account.ts
+++ src/types/account.ts
@@ -1,16 +1,29 @@
-export interface AccountBalance {
+import { BaseCustomer } from "./customer";
+
+interface AccountBalance {
   balance: number;
   tokenSymbol: string;
 }
 
-export interface Account {
+export interface Account extends Pick<BaseCustomer, 'createdAt' | 'updatedAt' | 'name'> {
   id: string;
-  createdAt: string;
-  updatedAt: string;
-  name: string;
   address: string;
   blockchain: string;
   balance: AccountBalance;
   isApiEnabled: boolean;
   isPending: boolean;
+  depositAccount: {
+    id: string;
+    accountId: string;
+    status: string;
+    currency: string;
+    bankBeneficiaryName: string;
+    bankBeneficiaryAddress: string;
+    bankName: string;
+    bankAddress: string;
+    bankRoutingNumber: string;
+    bankAccountNumber: string;
+    paymentRails: string[];
+  };
+  customer: BaseCustomer & { accountId: string };
 }
